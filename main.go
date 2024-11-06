0f530dfd270c20d6dc5381922fddb9093d0160b1
Dmitry Ostankov <dimjkee.97@gmail.com>
2024-11-06 22:55:10 +0300

    test

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func replaceLinks(mes string) string {
+	buffer := []byte(mes)  // буфер - байтовый срез
+	s := []byte("http://") // откуда маскировать
+
+	found := false // поиск начала ссылки
+	i := 0         // индекс, с которого начнётся замена
+	for i < len(buffer) {
+		for j := 0; j < len(s); j++ {
+			if buffer[j+i] == s[j] {
+				found = true
+			} else {
+				found = false
+				break
+			}
+		}
+		if found { // нашли http://
+			k := i + len(s) // текст ссылки
+			for k < len(buffer) && buffer[k] != ' ' {
+				buffer[k] = '*'
+				k++
+			}
+		}
+		i++
+	}
+	return string(buffer)
+}
+
+func main() {
+
+	text := "Hello, its my page: http://localhost123.com See you"
+	text1 := replaceLinks(text)
+	fmt.Println(text1)
+
+}
